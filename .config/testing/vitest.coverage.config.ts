7ad365eb1b50ec6d650238447202aed93bf76b23
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2026-03-26 12:48:54 -0500

    Properly configure test:coverage command

---
+++ .config/testing/vitest.coverage.config.ts
@@ -0,0 +1,39 @@
+import {defineConfig, mergeConfig} from 'vitest/config';
+import projectConfig from '../../vite.config';
+
+export default mergeConfig(
+  projectConfig,
+  defineConfig({
+    test: {
+      environment: 'happy-dom',
+      include: ['**/*.unit.ts', '**/*.unit.tsx', '**/*.integration.ts', '**/*.integration.tsx'],
+      exclude: ['**/.testing/**', '**/build/**', '**/node_modules/**'],
+      passWithNoTests: true,
+      globals: false,
+      reporters: ['default'],
+      coverage: {
+        provider: 'v8',
+        reporter: 'json-summary',
+        reportsDirectory: '.testing/coverage',
+        include: ['src/**/*.ts', 'src/**/*.tsx'],
+        exclude: [
+          '**/.testing/**',
+          '**/specs/**',
+          '**/*.bench.ts',
+          '**/*.bench.tsx',
+          '**/*.e2e.ts',
+          '**/*.e2e.tsx',
+          '**/*.integration.ts',
+          '**/*.integration.tsx',
+          '**/*.unit.ts',
+          '**/*.unit.tsx',
+          '**/build/**',
+          '**/index.ts',
+          '**/node_modules/**',
+          '**/register.d.ts',
+          '**/types/**',
+        ],
+      },
+    },
+  }),
+);
